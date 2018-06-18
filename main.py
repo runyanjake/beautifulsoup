395cbee0db6e174ffb9cd1637e5bda29caacd688
Jacob Runyan <jmrunyan@ucsc.edu>
2018-06-18 10:50:39 -0700

    Has found all new trades and pieced them out.

--- main.py
+++ main.py
@@ -10,7 +10,22 @@ from bs4 import BeautifulSoup as bs
 with open("./testhtml/rltrades.html") as file:
     soup = bs(file, "html5lib")
 
-    
+    title = soup.title
+    titlestr = soup.title.string
 
-    #close bs instance
+    print("Title is: " + str(titlestr))
+
+    index = 1
+    c = soup.find_all("div", {"class": "contents"}) #contents portion
+    for item in c: 
+        tradebox = item.find_all("div", {"class": "new"}) #the single tb holding trades
+        for newtrade in tradebox:
+            trade = newtrade.find_all("div", recursive=False)
+            link = trade[0]
+            haves = trade[1]
+            wants = trade[2]
+            time = trade[3]
+            print(str(index) + ": " + str(link) + "\n")
+            index = index + 1
+
     file.close()
