db8578cbca4194638dc195798730c12c123dc849
Jacob Runyan <jmrunyan@ucsc.edu>
2018-06-18 22:10:00 -0700

    Finished part 2 of the django tutorial, learned how the database works and stuff. Hopefully now can figure out how HTML and JS fit in

--- trader/trader/settings.py
+++ trader/trader/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trader-app.apps.NewappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'trader-app'
+    #'trader-app'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
