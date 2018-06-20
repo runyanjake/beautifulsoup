e9b04cf680bd0097e107dabec3b4292784e9f830
Jacob Runyan <jmrunyan@ucsc.edu>
2018-06-19 22:18:51 -0700

    Stopping Django for now, just gonna make a scraper that builds the database locally.

--- main.py
+++ main.py
@@ -7,6 +7,36 @@
 
 from bs4 import BeautifulSoup as bs
 
+# import requests
+# #Requesting Website with requests
+# r = requests.get("https://www.rl-trades.com/")
+# print("\n\n\n\n")
+# print("Status Code: " + str(r.status_code))
+# print("Text: " + str(r.text))
+# print("Content: " + str(r.content))
+# print("\n\n\n\n")
+
+import urllib
+#Requesting Website with urllib's urlopen
+# returns just "b''"?
+# with urllib.request.urlopen("http://www.rl-trades.com/") as response:
+#     html = response.read()
+#     print("\n\n\n\n")
+#     print(html)
+#     print("\n\n\n\n")
+
+
+#Requesting Website with urllib's urlretrive
+#returns: <_io.TextIOWrapper name='/var/folders/tw/5r3fybdx4s7412b_l445s1c00000gn/T/tmp6kdpa9rf' mode='r' encoding='UTF-8'>
+# local_filename, headers = urllib.request.urlretrieve("http://www.rl-trades.com/")
+# html = open(local_filename)
+# print("\n\n\n\n")
+# print(html)
+# print("\n\n\n\n")
+
+
+
+
 with open("./testhtml/rltrades.html") as file:
     soup = bs(file, "html5lib")
     index = 1
--- trader/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/runyanjake/Desktop/beautifulsoup/trader/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
